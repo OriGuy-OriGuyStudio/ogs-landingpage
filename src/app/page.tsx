c0133c2b417fd1259d778ed4db936e946a1c398e
Ori Guy Studio <origuy@origuystudio.com>
2025-04-05 12:19:21 +0200

    Update global styles and homepage layout; enhance color variables and typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-w-screen flex items-center  justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <h1 className="text-size-brand-h1 font-black text-color-brand-cyan-500-medium">
+        סטודיו אורי גיא
+      </h1>
     </div>
   );
 }
