49dcf73f165b18bfaa48ee4bef7971d5cff2f448
Ori Guy Studio <origuy@origuystudio.com>
2025-04-05 15:45:20 +0200

    feat: implement HeroSection component and update globals.css for theming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import HeroSection from "@/sections/hero/Hero";
+
 export default function Home() {
   return (
     <div className="min-w-screen flex items-center  justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1 className="text-size-brand-h1 font-black text-color-brand-cyan-500-medium">
         סטודיו אורי גיא
       </h1>
+      <HeroSection />
     </div>
   );
 }
---
+++ src/sections/hero/Hero.tsx
@@ -0,0 +1,11 @@
+interface HeroSectionProps {}
+
+const HeroSection = ({}: HeroSectionProps) => {
+  return (
+    <div className="">
+      <p className="text-size-brand-p">Hero Section</p>
+    </div>
+  );
+};
+
+export default HeroSection;
