eae20a2cb384c936808791acbe12772eb5d08f62
Ori Guy Studio <origuy@origuystudio.com>
2025-04-05 16:04:05 +0200

    feat: update layout for RTL support and enhance HeroSection with dynamic text

--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -1,9 +1,29 @@
+import { color } from "motion/react";
+
 interface HeroSectionProps {}
 
 const HeroSection = ({}: HeroSectionProps) => {
+  const texts = {
+    smallTitle: ":לאתר שלך יש תפקיד אחד",
+    mainTitle: "לגרום לאנשים לעצור, להרגיש, ולהגיד",
+    colorTitle: "זה בדיוק מה שחיפשתי",
+    description:
+      "אפיון, עיצוב ובניית אתרים לעסקים שמבינים שהאתר שלהם צריך לעבוד עבורם, להציג את הערך, לבלוט מהמתחרים ולייצר פניות.",
+    button: "רוצים אתר שמייצר לקוחות? לחצו כאן",
+    textUnderButton: "בלי התחייבות, רק שיחת היכרות",
+  };
   return (
-    <div className="">
-      <p className="text-size-brand-p">Hero Section</p>
+    <div className="text-center flex flex-col items-center justify-center">
+      <p className="text-size-brand-p">{texts.smallTitle}</p>
+      <h1 className="text-size-brand-h1 font-black">
+        {texts.mainTitle}{" "}
+        <span className="text-color-brand-pink-500-dark">
+          "{texts.colorTitle}"
+        </span>
+      </h1>
+      <h3>{texts.description}</h3>
+      <button>{texts.button}</button>
+      <p>{texts.textUnderButton}</p>
     </div>
   );
 };
