6b24d680a4d19176390c9614c2af5fc118ebcdb9
Ori Guy Studio <origuy@origuystudio.com>
2025-04-08 15:36:15 +0200

    feat: add prettier-plugin-tailwindcss for improved Tailwind CSS formatting; update layout and text in HeroSection

--- src/sections/hero/Hero.tsx
+++ src/sections/hero/Hero.tsx
@@ -1,43 +1,43 @@
-"use client";
-import HeroButton from "@/app/components/hero/HeroButton";
-import { Search } from "lucide-react";
-import { color } from "motion/react";
-import { use, useRef } from "react";
-import { text } from "stream/consumers";
+'use client';
+import HeroButton from '@/app/components/hero/HeroButton';
+import { Search } from 'lucide-react';
+import { color } from 'motion/react';
+import { use, useRef } from 'react';
+import { text } from 'stream/consumers';
 
 interface HeroSectionProps {}
 
 const HeroSection = ({}: HeroSectionProps) => {
   const textToScale = useRef<HTMLSpanElement>(null);
   const texts = {
-    smallTitle: "לאתר שלך יש תפקיד אחד:",
-    mainTitle: "לגרום לאנשים לעצור, להרגיש, ולהגיד",
-    colorTitle: "זה בדיוק מה שחיפשתי",
+    smallTitle: 'לאתר שלך יש תפקיד אחד:',
+    mainTitle: 'לגרום לאנשים לעצור, להרגיש, ולהגיד',
+    colorTitle: 'זה בדיוק מה שחיפשתי',
     description:
-      "אפיון, עיצוב ובניית אתרים לעסקים שמבינים שהאתר שלהם צריך לעבוד עבורם, להציג את הערך, לבלוט מהמתחרים ולייצר פניות.",
-    button: "רוצים אתר שמייצר לקוחות?",
-    textUnderButton: "בלי התחייבות, רק שיחת היכרות",
+      'אפיון, עיצוב ובניית אתרים לעסקים שמבינים שהאתר שלהם צריך לעבוד עבורם, להציג את הערך, לבלוט מהמתחרים ולייצר פניות.',
+    button: 'רוצים אתר שמייצר לקוחות?',
+    textUnderButton: 'בלי התחייבות, רק שיחת היכרות',
   };
   return (
-    <div className="text-right text-color-brand-whiteYellow-100-light sm:text-center leading-none">
-      <div className="bg-color-brand-purple-500-light w-60 h-60 blur-[800px] -z-10 absolute left-0 top-0" />
-      <div className="bg-color-brand-pink-500-dark w-60 h-60 blur-[800px] -z-10 absolute right-0 top-0" />
-      <div className="bg-color-brand-pink-500-dark w-60 h-60 blur-[800px] -z-10 absolute left-0 bottom-0" />
-      <div className="bg-color-brand-purple-500-light w-60 h-60 blur-[800px] -z-10 absolute right-0 bottom-0" />
+    <div className="text-color-brand-whiteYellow-100-light text-right leading-none sm:text-center">
+      <div className="bg-color-brand-purple-500-light absolute top-0 left-0 -z-9 h-60 w-60 blur-[800px]" />
+      <div className="bg-color-brand-pink-500-dark absolute top-0 right-0 -z-9 h-60 w-60 blur-[800px]" />
+      <div className="bg-color-brand-pink-500-dark absolute bottom-0 left-0 -z-9 h-60 w-60 blur-[800px]" />
+      <div className="bg-color-brand-purple-500-light absolute right-0 bottom-0 -z-9 h-60 w-60 blur-[800px]" />
       <p className="text-size-brand-p">{texts.smallTitle}</p>
-      <h1 className="text-size-brand-h2 sm:text-size-brand-h1 font-black mb-2 ">
-        {texts.mainTitle}{" "}
+      <h1 className="text-size-brand-h2 sm:text-size-brand-h1 mb-2 font-black">
+        {texts.mainTitle}{' '}
         <span
           ref={textToScale}
-          className=" relative magnify-container text-color-brand-pink-500-dark block "
+          className="magnify-container text-color-brand-pink-500-dark relative block"
         >
           "{texts.colorTitle}"
           <span>
-            <Search size={60} className="absolute -top-1 -right-3 " />
+            <Search size={60} className="absolute -top-1 -right-3" />
           </span>
         </span>
       </h1>
-      <h3 className="text-size-brand-h6 leading-snug mb-8 w-full sm:w-5/8 mx-auto">
+      <h3 className="text-size-brand-h6 mx-auto mb-8 w-full leading-snug sm:w-5/8">
         {texts.description}
       </h3>
       <HeroButton text={texts.button} />
