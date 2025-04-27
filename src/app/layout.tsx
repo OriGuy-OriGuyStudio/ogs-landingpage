cebffa35ac3686ba82e8acd5cc59816cbb2f1233
Ori Guy Studio <origuy@origuystudio.com>
2025-04-27 19:15:26 +0200

    add google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Rubik } from 'next/font/google';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 export const metadata: Metadata = {
   title: 'הלקוחות שלך יגידו: זה בדיוק מה שחיפשתי',
@@ -16,7 +17,15 @@ export default function RootLayout({
     <html dir="rtl" lang="he">
       <body className={`overflow-x-hidden ${rubik.className}`}>
         {children}
-
+        <GoogleTagManager gtmId={'GTM-TDKTJVBL'} />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TDKTJVBL"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <script
           src="https://cdn.jsdelivr.net/npm/@tsparticles/confetti@3.0.3/tsparticles.confetti.bundle.min.js"
           async
